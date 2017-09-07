a459706ed54b3761e41f116056351d192e6070e5
yumiao136 <31519474+yumiao136@users.noreply.github.com>
2017-09-07 17:40:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ console.log('hello little little world!')
 document.write('<h1> Hi!</h1>')
 
 
+
+
 place_list = ["here","there","nowhere","lala land","caribbean"];
 
 "I am from" + " " + place_list[Math.floor(Math.random()*place_list.length)] + "," + "and he is from " + " " + place_list[Math.floor(Math.random()*place_list.length)] + "."
+
+
+
+
+
+
+
+
+
+
+
+place_list = ["here","there","nowhere","lala land","caribbean"];
+
+let place = prompt("Where are you from?")
+
+"You are from" + " " + place + "," + " " + "and he is from " + " " + place_list[Math.floor(Math.random()*place_list.length)] + "."
