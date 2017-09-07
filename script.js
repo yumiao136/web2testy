57abab9146ee1e9ef20c0d1c6dfbb4bb3aec14a8
yumiao136 <31519474+yumiao136@users.noreply.github.com>
2017-09-07 17:01:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('hello little little world!')
 document.write('<h1> Hi!</h1>')
 
-place_list = ["here","there","nowhere","lala land","caribbean"]
 
-"I am from" + " " + place_list[Math.floor(Math.random()*place_list.length)] + "," + "and he is from " + " " + place_list[Math.floor(Math.random()*place_list.length)] + "."
+place_list = ["here","there","nowhere","lala land","caribbean"];
+
+var sentence = "I am from" + " " + place_list[Math.floor(Math.random()*place_list.length)] + "," + "and he is from " + " " + place_list[Math.floor(Math.random()*place_list.length)] + ".";
